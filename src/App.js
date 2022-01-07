e16d1104f100dcf7bfaf457b9f01bd237da2366b
leonardoaguirre <62442754+leonardoaguirre@users.noreply.github.com>
2022-01-07 18:42:11 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Image, StyleSheet, View, Text } from 'react-native';
+import foto from './assets/profile.jpg'
+
+const App = () => {
+    return (
+        <>
+            
+        </>
+    );
+};
+
+
+export default App;
