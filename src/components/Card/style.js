16131013a83babb6c070ab481371110ca824a0b1
leonardoaguirre <62442754+leonardoaguirre@users.noreply.github.com>
2022-01-07 22:22:29 -0300

    Cria componente card e seu estilo

---
+++ src/components/Card/style.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+    card_container: {
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    card: {
+        width: '100%',
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: '#939393',
+        padding: 10,
+        backgroundColor: '#FFF'
+    },
+    card_header: {
+
+    },
+    card_content: {
+    },
+    card_header_text: {
+        marginBottom: 10,
+        fontSize : 24,
+        color: '#939393',
+        fontWeight: 'bold',
+        color: '#000'
+    },
+})
